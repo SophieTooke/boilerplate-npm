803d63b3304c49ab7df4c911e2ccb76199db8c67
SophieTooke <soph.tooke@gmail.com>
2023-07-25 17:16:26 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let app = express();
 
+// app.get("/", function(req, res) {
+//     res.send("Hello Express");
+// })
+
+let __pages = '/views/'
+
 app.get("/", function(req, res) {
-    res.send("Hello Express");
+    res.send(_pages + 'index.html');
 })
 
 
